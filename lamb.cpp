4079a94a402dc9c578316e2322b5b63472a3e1f6
august <august@shitbox>
2025-09-25 15:30:08 -0500

    added the GNU public license recommended notices to lamb.cpp

--- lamb.cpp
+++ lamb.cpp
@@ -8,12 +8,23 @@
 
 using namespace std;
 
-      ////////////////////////
-     // nukenukey @ github //
-    // Mar 07 25          //
-   ////////////////////////
+// compiler for lamb syntax to nasm (the netwide assembler) to linux for x86-64 machines
+// Copyright (C) 2025  Michael Burdick
 
- // lamb -> nasm for linux for 64 bit
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// feel free to me at <micklebubble22@gmail.com> if you have questions
 
 string specChars[] = { ":", ";", "(", ")", " ", "\t", "!", ",", ".", "\'", "\"", "`", "&" };
 bool show = false;
